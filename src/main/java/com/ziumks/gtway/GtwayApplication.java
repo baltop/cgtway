767bea15654f4fb89899ce13a15592179b66a8f8
baltop <baltop@gmail.com>
2020-08-17 16:26:57 +0900

    scrap

--- src/main/java/com/ziumks/gtway/GtwayApplication.java
+++ src/main/java/com/ziumks/gtway/GtwayApplication.java
@@ -2,6 +2,8 @@ package com.ziumks.gtway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import reactor.core.publisher.Mono;
 
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class GtwayApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("reactor.netty.http.server.accessLogEnabled", "true");
 		SpringApplication.run(GtwayApplication.class, args);
 	}
 
