094f6e123709fa1e0b116ce027e5d4c1a8c72025
baltop <baltop@gmail.com>
2020-08-21 02:15:56 +0900

    jwt filter add

---
+++ src/main/java/com/ziumks/gtway/JwtRequestFilter.java
@@ -0,0 +1,28 @@
+package com.ziumks.gtway;
+
+import org.slf4j.Logger; import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component  
+public class JwtRequestFilter implements GlobalFilter {
+  final Logger logger = LoggerFactory.getLogger(JwtRequestFilter.class);
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+      try {
+          String token = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7);
+          logger.info("+++++++++++++++++++++++++"+token);
+      } catch (NullPointerException e) {
+          logger.warn("no token.");
+          exchange.getResponse().setStatusCode(HttpStatus.valueOf(401));
+          logger.info("status code :" + exchange.getResponse().getStatusCode());
+          return chain.filter(exchange);
+      }
+      return chain.filter(exchange);
+  }
+}
